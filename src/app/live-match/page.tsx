ea28b9725ebdbb3df9e374ea247d4641f16e7640
tiehisung <tiehisungai@gmail.com>
2025-10-14 09:00:24 +0000

    fixut

--- src/app/live-match/page.tsx
+++ src/app/live-match/page.tsx
@@ -1,21 +1,19 @@
 import React from "react";
 import { getLiveMatch } from "../admin/live-match/page";
-import { MatchEvents } from "../admin/live-match/(components)/Events";
+// import { MatchEvents } from "../admin/live-match/(components)/Events";
 import { Title } from "@/components/Elements";
-import { LiveMatchCard } from "./Live";
+// import { LiveMatchCard } from "./Live";
 
-const LiveMatchPage = async () => {
-  const match = await getLiveMatch();
+export default async function LiveMatchPage() {
+  // const match = await getLiveMatch();
   return (
     <div className="pt-4 container">
       <Title>Live Match Page</Title>
       <div className="grid gap-6">
-        <LiveMatchCard match={match} />
+        {/* <LiveMatchCard match={match} />
 
-        <MatchEvents match={match} className="max-h-fit" />
+        <MatchEvents match={match} className="max-h-fit" /> */}
       </div>
     </div>
   );
-};
-
-export default LiveMatchPage;
+}
--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import FixturesSection from "./Fixtures";
-import MatchesSection from "./Matches";
+// import FixturesSection from "./Fixtures";
+// import MatchesSection from "./Matches";
 
-import { getMatches } from "../admin/matches/page";
-import { IMatchProps } from "./(fixturesAndResults)";
+// import { getMatches } from "../admin/matches/page";
+// import { IMatchProps } from "./(fixturesAndResults)";
 
 export default async function MatchesPage() {
-  const allMatches: IMatchProps[] = await getMatches();
+  // const allMatches: IMatchProps[] = await getMatches();
 
-  console.log({ allMatches });
-  const playedMatches = allMatches?.filter((match) => match.status == "FT");
-  const fixtures = allMatches?.filter((match) => match.status !== "FT");
+  // console.log({ allMatches });
+  // const playedMatches = allMatches?.filter((match) => match.status == "FT");
+  // const fixtures = allMatches?.filter((match) => match.status !== "FT");
   return (
     <div className="space-y-8">
-      <FixturesSection fixtures={fixtures} />
-      <MatchesSection matches={playedMatches} />
+      {/* <FixturesSection fixtures={fixtures} />
+      <MatchesSection matches={playedMatches} /> */}
     </div>
   );
 }
