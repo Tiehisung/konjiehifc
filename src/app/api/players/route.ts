d42254f3fb967a7e8b40b1dde9fac9068131dc6e
Alhassan Ibrahim <tiehisungai@gmail.com>
2026-01-10 08:04:18 +0000

    player card

--- src/app/api/players/route.ts
+++ src/app/api/players/route.ts
@@ -11,14 +11,13 @@ import { EUserRole } from "@/types/user";
 import { formatDate } from "@/lib/timeAndDate";
 import { IPostPlayer } from "@/types/player.interface";
 
-
 ConnectMongoDb();
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const page = Number.parseInt(searchParams.get("page") || "1", 10);
 
-  const isActive = searchParams.get("isActive") == "0" ? false : true
+  const isActive = searchParams.get("isCurrentPlayer") == "0" ? false : true
 
   const limit = Number.parseInt(searchParams.get("limit") || "30", 10);
   const skip = (page - 1) * limit;
@@ -39,7 +38,7 @@ export async function GET(request: NextRequest) {
       { "email": regex },
       { "status": regex },
     ],
-    isActive: isActive,
+    isCurrentPlayer: isActive,
     status
   }
   const cleaned = removeEmptyKeys(query)
